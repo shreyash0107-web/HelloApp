8aaeb7fb981a40f537837722cd1dc2bddab8f82a
Shreyash Kumar <149252068+shreyash0107-web@users.noreply.github.com>
2026-03-25 19:29:42 +0530

    Made Changes in HelloApp

--- src/main/Java/HelloAppUC5.java
+++ src/main/Java/HelloAppUC5.java
@@ -1,11 +1,9 @@
 public class HelloAppUC5 {
     public static void main(String[] args) {
 
-        // Check if no arguments are passed
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            // Enhanced for loop to iterate through arguments
             for (String name : args) {
                 System.out.println("Hello, " + name + "!");
             }
