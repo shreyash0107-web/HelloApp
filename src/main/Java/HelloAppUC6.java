89536fb5a21dc4f6df83bc1b53e79bb4e54d4189
Shreyash Kumar <149252068+shreyash0107-web@users.noreply.github.com>
2026-03-25 19:37:05 +0530

    Made Changes in HelloApp

--- src/main/Java/HelloAppUC6.java
+++ src/main/Java/HelloAppUC6.java
@@ -1,21 +1,21 @@
 public class HelloAppUC6 {
     public static void main(String[] args) {
 
-        // If no arguments → default message
+        
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
             String result = "";
 
-            // Build string with delimiter
+            
             for (String name : args) {
                 result += name + ", ";
             }
 
-            // Remove trailing ", " using substring
+            
             result = result.substring(0, result.length() - 2);
 
-            // Print final output
+            
             System.out.println("Hello, " + result + "!");
         }
     }
